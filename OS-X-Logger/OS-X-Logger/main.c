7a9634de4f6c9b789e1df42b8a9c6e2d9c342209
Eltrien <Kwakch@yandex.ru>
2016-03-09 07:38:31 -0800

    1st commit
    main hello world!

---
+++ OS-X-Logger/OS-X-Logger/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  OS-X-Logger
+//
+//  Created by Eltrien on 3/8/16.
+//  Copyright (c) 2016 Eltrien. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
